b92055e1c1dd931505c0065b1adc906c47965a55
kjjeon <claret8313@gmail.com>
2020-05-26 20:59:56 +0900

    Feature/func1 (#1)
    * add int 3
    
    * add int b
    
    Co-authored-by: joel <joel@afreecatv.com>

--- app/src/main/java/com/example/jenny/gittest/MainActivity.java
+++ app/src/main/java/com/example/jenny/gittest/MainActivity.java
@@ -9,8 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a = 4;
+        int a = 3;
 
+        int b = 0;
         //릴리즈 1.0.0 끝!
         //핫픽스 1.0.1
         //asdf
